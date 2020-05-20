9fb92b1c1e942474a9f3315d3e2db5fbeff158e0
stephanyvargas <yostephy@hotmail.com>
2020-05-20 17:19:20 +0900

    expanded side explanation notes, intregration from 1 not working...

--- reconstruction_square_pulse.py
+++ reconstruction_square_pulse.py
@@ -3,33 +3,44 @@ import numpy as np
 
 tau = 1
 delta = 0.01
+
+
+def calculate_integral(dy):
+    I_dy = np.sum(dy*(1/100))
+    I_uy = np.sum(uy*(9/900))
+    print('Droop integral:', I_dy, '/n', 'Undershoot integral:', I_uy)
+
+
+#artificial square pulse algorithm
+up_y = np.linspace(1,1,100)
+low_y = np.linspace(0,0,900)
+square_pulse = np.concatenate(up_y, low_y)
+
 #upper part of the pulse for the droop, lower for the undershoot
 up_x = np.linspace(0,1,100)
 low_x = np.linspace(1,10,900)
 
-
-#deformed pulse
+#deformed the pulse by a tau constant
 dy = np.exp(-up_x/tau)
 uy = (np.exp(-delta/tau)-1)*np.exp(-(low_x-1)/tau)
 x = np.linspace(0,10,1000)
 y = np.concatenate((dy,uy))
 
 
-#integral of both parts
-#between 0 to 10 the integrals are: 0.787103603082587 -0.7793227436228078
-#between 0 to 100 the integrals are: 0.787103603082587 -0.7888281781410758
-I_dy = np.sum(dy*(1/100))
-I_uy = np.sum(uy*(9/900))
+#inital check for both integrals (droop and undershoot)
+'''between 0 to 10 the integrals are: (0 to 1) 0.787103603082587, (0 to 10) -0.7793227436228078
+between 0 to 100 the integrals are: (0 to 1) 0.787103603082587, (0 to 100) -0.7888281781410758'''
+#calculate_integral(dy)
 
 
-#Algorithm to reconstruct by slices the pulse
+#Algorithm to reconstruct by slices the pulse using single tau approximation
 A = tau * (1-np.exp(-delta/tau))
 f0 = A
 Sj = [0]
 X0 = (1/A)*uy[0]
 X = [X0]
 
-for i in range(1, len(low_x)):
+for i in range(0, len(low_x)):
     sj = X[i-1] + np.exp(-delta/tau)*Sj[i-1]
     Sj.append(sj)
     xj = (1/A)*low_x[0] + (A/tau)*sj
@@ -38,6 +49,7 @@ for i in range(1, len(low_x)):
 #print('value of sj', Sj)
 #print('value of xj', X, '\n')
 
+plt.plot(x, )
 plt.plot(low_x, X)
 plt.plot(x,y)
 plt.grid(linestyle='dotted')
