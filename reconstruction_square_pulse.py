87693df3b3f40600521b27a416d4bc02cd47db7b
stephanyvargas <yostephy@hotmail.com>
2020-05-21 15:34:20 +0900

    artificial square pulse included in the plot

--- reconstruction_square_pulse.py
+++ reconstruction_square_pulse.py
@@ -1,20 +1,21 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-tau = 1
+tau = 1.5
 delta = 0.01
+width = 1
 
 
-def calculate_integral(dy):
+def calculate_integral(dy, uy):
     I_dy = np.sum(dy*(1/100))
     I_uy = np.sum(uy*(9/900))
     print('Droop integral:', I_dy, '/n', 'Undershoot integral:', I_uy)
 
 
 #artificial square pulse algorithm
-up_y = np.linspace(1,1,100)
-low_y = np.linspace(0,0,900)
-square_pulse = np.concatenate(up_y, low_y)
+up_y = np.ones(100)
+low_y = np.zeros(900)
+square_pulse = np.concatenate((up_y, low_y))
 
 #upper part of the pulse for the droop, lower for the undershoot
 up_x = np.linspace(0,1,100)
@@ -22,35 +23,41 @@ low_x = np.linspace(1,10,900)
 
 #deformed the pulse by a tau constant
 dy = np.exp(-up_x/tau)
-uy = (np.exp(-delta/tau)-1)*np.exp(-(low_x-1)/tau)
+uy = (np.exp(-width/tau)-1)*np.exp(-(low_x-1)/tau)
 x = np.linspace(0,10,1000)
 y = np.concatenate((dy,uy))
 
 
 #inital check for both integrals (droop and undershoot)
-'''between 0 to 10 the integrals are: (0 to 1) 0.787103603082587, (0 to 10) -0.7793227436228078
-between 0 to 100 the integrals are: (0 to 1) 0.787103603082587, (0 to 100) -0.7888281781410758'''
-#calculate_integral(dy)
+'''#between 0 to 10 the integrals are: (0 to 1) 0.787103603082587, (0 to 10) -0.7793227436228078
+#between 0 to 100 the integrals are: (0 to 1) 0.787103603082587, (0 to 100) -0.7888281781410758
+'''
+calculate_integral(dy, uy)
 
 
 #Algorithm to reconstruct by slices the pulse using single tau approximation
-A = tau * (1-np.exp(-delta/tau))
+A = tau * (1-np.exp(-width/tau))
 f0 = A
 Sj = [0]
-X0 = (1/A)*uy[0]
+X0 = (1/A)*dy[0]
 X = [X0]
 
-for i in range(0, len(low_x)):
+for i in range(1, len(x)):
+    #t0 = 
+
     sj = X[i-1] + np.exp(-delta/tau)*Sj[i-1]
     Sj.append(sj)
-    xj = (1/A)*low_x[0] + (A/tau)*sj
+    xj = (1/A)*x[i] + (A/tau)*sj
     X.append(xj*0.01)
 
 #print('value of sj', Sj)
 #print('value of xj', X, '\n')
 
-plt.plot(x, )
-plt.plot(low_x, X)
-plt.plot(x,y)
+plt.plot(x, square_pulse, label='original square pulse')
+plt.plot(x,y, label=r'drooped pulse with $\tau=$ {}'.format(tau))
+plt.plot(x, X, label='Droop correction')
+plt.xlabel('Time')
+plt.ylabel('Voltage')
+plt.legend(loc='best')
 plt.grid(linestyle='dotted')
 plt.show()
